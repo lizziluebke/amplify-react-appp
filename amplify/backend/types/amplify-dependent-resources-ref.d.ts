23a2b0682a53c16d7368c17c2cf3f7412138827e
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-04-22 17:25:00 -0500

    amplify push and tested in console

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "cryptofunctionspring": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "cryptoapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
